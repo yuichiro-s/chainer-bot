6abbcc5451319a4e2a1eb771e65b900084379b8a
yuichiro-s <sawai.yuichiro.sn0@is.naist.jp>
2015-10-05 00:12:01 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='bot',
+    version='0.0.1',
+    description='RNN-based chatbot',
+    author='Yuichiro Sawai',
+    author_email='sawai.yuichiro.sn0@is.naist.jp',
+    packages=['bot'],
+)
