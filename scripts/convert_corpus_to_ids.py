25b5293bba489c8e32651a12b204c41d4c6c3060
yuichiro-s <sawai.yuichiro.sn0@is.naist.jp>
2015-10-10 03:55:58 +0900

    Update script to convert corpus into list of IDs

--- scripts/convert_corpus_to_ids.py
+++ scripts/convert_corpus_to_ids.py
@@ -23,19 +23,22 @@ def main(args):
                     ws = line
                 else:
                     ws = line.split()
-                w_ids = map(voc.get_id, ws)
+                ids_str = ' '.join(map(lambda w: str(voc.get_id(w)), ws))
 
                 if src is None:
-                    src = w_ids
+                    src = ids_str
                 else:
-                    trg = w_ids
+                    trg = ids_str
 
-                    f_out.write(' '.join(map(str, src)))
+                    f_out.write(src)
                     f_out.write('\t')
-                    f_out.write(' '.join(map(str, trg)))
+                    f_out.write(trg)
                     f_out.write('\n')
 
-                    src = None
+                    if args.dialog:
+                        src = trg
+                    else:
+                        src = None
 
 
 if __name__ == '__main__':
@@ -48,5 +51,6 @@ if __name__ == '__main__':
     parser.add_argument('dest', help='destination of converted corpus')
     parser.add_argument('vocab', help='vocabulary file')
     parser.add_argument('--char', action='store_true', default=False, help='segment sentences character-wise')
+    parser.add_argument('--dialog', action='store_true', default=False, help='use target sequence as source sequence for next sample')
 
     main(parser.parse_args())
